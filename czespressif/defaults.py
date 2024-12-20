e487d5432e07a46b32d5711dd5450ee43cf8eaca
Peter Dragun <peter.dragun@espressif.com>
2024-12-20 15:12:05 +0100

    fix: Update shields to link directly to desired website
    Before this fix, shields were linked to a new window with just a shield.
    A second click was needed to actually get to the desired website.

--- czespressif/defaults.py
+++ czespressif/defaults.py
@@ -101,9 +101,15 @@ CHANGELOG_HEADER: str = """
 > This list is not exhaustive - only important changes, fixes, and new features in the code are reflected here.
 
 <div align="center">
-    <img alt="Static Badge" src="https://img.shields.io/badge/Keep%20a%20Changelog-v1.1.0-salmon?logo=keepachangelog&logoColor=black&labelColor=white&link=https%3A%2F%2Fkeepachangelog.com%2Fen%2F1.1.0%2F">
-    <img alt="Static Badge" src="https://img.shields.io/badge/Conventional%20Commits-v1.0.0-pink?logo=conventionalcommits&logoColor=black&labelColor=white&link=https%3A%2F%2Fwww.conventionalcommits.org%2Fen%2Fv1.0.0%2F">
-    <img alt="Static Badge" src="https://img.shields.io/badge/Semantic%20Versioning-v2.0.0-grey?logo=semanticrelease&logoColor=black&labelColor=white&link=https%3A%2F%2Fsemver.org%2Fspec%2Fv2.0.0.html">
+    <a href="https://keepachangelog.com/en/1.1.0/">
+        <img alt="Static Badge" src="https://img.shields.io/badge/Keep%20a%20Changelog-v1.1.0-salmon?logo=keepachangelog&logoColor=black&labelColor=white&link=https%3A%2F%2Fkeepachangelog.com%2Fen%2F1.1.0%2F">
+    </a>
+    <a href="https://www.conventionalcommits.org/en/v1.0.0/">
+        <img alt="Static Badge" src="https://img.shields.io/badge/Conventional%20Commits-v1.0.0-pink?logo=conventionalcommits&logoColor=black&labelColor=white&link=https%3A%2F%2Fwww.conventionalcommits.org%2Fen%2Fv1.0.0%2F">
+    </a>
+    <a href="https://semver.org/spec/v2.0.0.html">
+        <img alt="Static Badge" src="https://img.shields.io/badge/Semantic%20Versioning-v2.0.0-grey?logo=semanticrelease&logoColor=black&labelColor=white&link=https%3A%2F%2Fsemver.org%2Fspec%2Fv2.0.0.html">
+    </a>
 </div>
 <hr>
 """
